91c4f04857c1238571a8eae728785d814c3d3892
Lam Nguyen <lamcnguyen89@gmail.com>
2020-12-06 01:50:43 -0500

    Styled the JQuery divs that will display artwork from the museums. Also cleaned up the code somewhat.

--- public/scripts/harvardApi.js
+++ public/scripts/harvardApi.js
@@ -14,13 +14,18 @@ $("#searchBtn").on("click", function() {
 
 });
 
-function searchArtist(artist) {  
-        storeArtist(artist); 
+function searchArtist(artist, isSearchAgain = false) {  
+        storeArtist(artist);
+        
+        if(isSearchAgain == true  && artistKey == null || artistKey.length == 0){ //artistKey is null when doing searchAgain
+            artistKey = artist;
+        }
 
         $(".gallery").empty()
         var queryHarvardURL = baseHarvardURL + artist +"&size=100&apikey=" + harvardAPIkey;
         console.log("Harvard API URL: " + queryHarvardURL)
 
+        // Start of API Pull Request
         $.ajax({
             url: queryHarvardURL,
             method: "GET"     
@@ -30,15 +35,12 @@ function searchArtist(artist) {
 
             console.log("Object Url: " + queryHarvardURL);
             console.log("Records length: " + response.records.length);
-   //Hit F12 instead of Inspect to debug the program at large not just an area of on the page
             
             if(response.records.length == 0){
                 $(".gallery").append("<p>").addClass("alerts").css("color","white").text("Artist not found. Be sure to use First and Last Name");   
             }
 
-            console.log("Record item example: " + JSON.stringify(response.records[0].url));
-            
-
+ 
             // This loop will theoretically generate images by pulling down from the server:
             for (let i = 0; i < response.records.length; i++) {
                     var harvardImages = response.records[i];
@@ -46,28 +48,43 @@ function searchArtist(artist) {
                     var baseImage = harvardImages.baseimageurl;
                     // var urlImage = harvardImages.url;
                     var artTitle = harvardImages.title;
-
                     var artistName = harvardImages.people[0].name;
                     var artistCulture = harvardImages.people[0].culture; 
 
                     // Create function to generate elements:
                     function createElements() {
-                        var mainImgDiv = $("<div>").attr("class","desc" );
-                        mainImgDiv.append("Artist Name: " + artistName)
-                        mainImgDiv.append("<hr>")
-                        mainImgDiv.append("Artwork Title: " + artTitle)
-                        mainImgDiv.append("<hr>")
-                        mainImgDiv.append("Artist Ethnicity: " + artistCulture)
-                        mainImgDiv.append(artImageSrc)
+
+                        // Create Elements for the image card:
+                        var mainImgDiv = $("<div>").attr("class","card m-3" );
+                        var mainImgCardBody = $("<div>").attr("class","card-body");
+                        var cardTitle=$("<h5>").attr("class","card-title").text("Artwork Title: " + artTitle);
+                        var cardName=$("<p>").attr("class","card-text").text("Artist Name: " + artistName);
+                        var cardCulture=$("<p>").attr("class","card-text").text("Artist Ethnicity: " + artistCulture);
+                        
+                        // Append Information to the Card Text Box:
+                        mainImgCardBody.append([
+                            cardTitle,
+                            cardName,
+                            cardCulture
+                        ]);
+                        
+                        // Append Image and Card Text Box to the Main Card:
+                        mainImgDiv.append([
+                            artImageSrc,
+                            mainImgCardBody
+                        ]);
+
+                        // Append Card to the a Div element created on the html page.
                         $(".gallery").append(mainImgDiv);
                     };
+
                     // Have to use this if else statement because Harvard stores images in 3 possible keys:
                     if (primaryImage) {
-                        var artImageSrc = $("<img>").attr("src", primaryImage).attr("class", "galleryImg");
+                        var artImageSrc = $("<img>").attr("src", primaryImage).attr("class", "card-img-top");
                         createElements();
                         
                     } else if (baseImage) {
-                        var artImageSrc = $("<img>").attr("src", baseImage).attr("class", "galleryImg");
+                        var artImageSrc = $("<img>").attr("src", baseImage).attr("class", "card-img-top");
                         createElements();
                         
                     } else {
--- public/scripts/metropolitanApi.js
+++ public/scripts/metropolitanApi.js
@@ -1,3 +1,5 @@
+var baseMetropolitanURL = "https://collectionapi.metmuseum.org/public/collection/v1/search?q="; // For Getting Object IDs associated with a particular artist
+var MetropolitanImageURL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" // For extracting images and the artwork information from each particulat Object ID belonging to that artist.
 let artistKey = "";
 
 
@@ -13,74 +15,71 @@ $("#searchBtn").on("click", function() {
 
 function searchArtist(artist, isSearchAgain = false) { //es6 uses C# default paramenters, calling function can override
 
-
   storeArtist(artist); 
 
   if(isSearchAgain == true  && artistKey == null || artistKey.length == 0){ //artistKey is null when doing searchAgain
     artistKey = artist;
   }
   
-
   $(".gallery").empty()
-  console.log("$$$$ artistKey = " + artistKey);
-  var queryURLMet = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=" + artistKey;
+  var queryURLMet = baseMetropolitanURL + artistKey;
+  
 // Start of API Pull Request
 $.ajax({
     url: queryURLMet,
     method: "GET"
   }).then(function(response) {
 
-    var results = response.objectIDs;
-    // var resultImage = results[0];
-    console.log("Search Results - " + results);
+        var objectID = response.objectIDs; // This is an object containing the ID Numbers for the works of a particular artist.
+
+        // Because of the way the Metropolitan Museum of Art API is set up, we have to do 2 sets of ajax requests:
+          // The first request was to get all the object IDs associated with the particular artist.
+          // The second set of requests is where we have to loop through the list of object IDs, make a single request for each ID, and then get the info we need.
+        
+        // The loop to get the images and data for the artist. We're only doing a maximum of 50 images because there are often hundreds of images
         for (let i = 0; i < 50 ; i++) {
         
-    
-    $.ajax({
-        url: "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + results[i],
-        method: "GET"
-      }).then(function(response) {
-
-              if(response.length == 0){
-                $(".gallery").append("<p>").addClass("alerts").css("color","white").text("Artist not found. Be sure to use First and Last Name");   
-              }
+            $.ajax({
+              url: MetropolitanImageURL + objectID[i],
+              method: "GET"
+            })
             
-              var results = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
-              console.log("image to be displayed: " + response.primaryImage);
-              var artistName = response.artistDisplayName;
-          
-              console.log("Artist Name: " + artistName + " artist = #" + artist +"#");  
-              var artTitle = response.title;
-              console.log("Art Title: " + artTitle);
-              var artistCulture = response.culture;
-              console.log("Artist Origin: " + artistCulture);
-              
-              
-              var artImageURL = response.primaryImage;
-              var artImageSrc = $("<img>").attr("src", artImageURL).attr("class", "galleryImg");
+            .then(function(response) {
 
-              // creating divs to show results side by side
-                  
-              var mainImgDiv = $("<div>").attr("class","desc" );
-              
-              mainImgDiv.append("Artist Name: " + artistName)
-              mainImgDiv.append("<hr>")
-              mainImgDiv.append("Artwork Title: " + artTitle)
-              mainImgDiv.append("<hr>")
-              mainImgDiv.append("Artist Origin: " + artistCulture)
-              mainImgDiv.append(artImageSrc)
-              $(".gallery").append(mainImgDiv);
-            
-            
+                    if(response.length == 0){
+                      $(".gallery").append("<p>").addClass("alerts").css("color","white").text("Artist not found. Be sure to use First and Last Name");   
+                    }
 
-      });
+                    // Variables that contain the artist information taken from the Metropolitan API"
+                    var artTitle = response.title;
+                    var artistName = response.artistDisplayName;
+                    var artistCulture = response.culture;
+                    var artImageURL = response.primaryImage;
 
+                    // Create Elements for the image card:
+                    var mainImgDiv = $("<div>").attr("class","card m-3" );
+                    var mainImgCardBody = $("<div>").attr("class","card-body");
+                    var cardTitle=$("<h5>").attr("class","card-title").text("Artwork Title: " + artTitle);
+                    var cardName=$("<p>").attr("class","card-text").text("Artist Name: " + artistName);
+                    var cardCulture=$("<p>").attr("class","card-text").text("Artist Ethnicity: " + artistCulture);
+                    var artImageSrc = $("<img>").attr("src", artImageURL).attr("class", "card-img-top");
+
+                    // Append Information to the Card Text Box:
+                    mainImgCardBody.append([
+                        cardTitle,
+                        cardName,
+                        cardCulture
+                    ]);
+                                          
+                    // Append Image and Card Text Box to the Main Card:
+                    mainImgDiv.append([
+                        artImageSrc,
+                        mainImgCardBody
+                    ]);    
+
+                    // Append Card to the a Div element created on the html page.
+                    $(".gallery").append(mainImgDiv);
+                });
         }        
-        
-
   });
-
-
-
-
 };
