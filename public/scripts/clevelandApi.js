4937c8feed66ee2356aef61addd61fb27611bb58
Lam Nguyen <lamcnguyen89@gmail.com>
2020-12-06 21:27:21 -0500

    Got the Cleveland museum page to mostly work

--- public/scripts/clevelandApi.js
+++ public/scripts/clevelandApi.js
@@ -29,10 +29,55 @@ function searchArtist(artist, isSearchAgain = false) {
     })
 
     .then(function(response) {
-        
-        if(response.length == 0){
+
+        console.log(response.data)
+
+        if(response.data.length == 0){
             $(".gallery").append("<p>").addClass("alerts").css("color","white").text("Artist not found. Be sure to use First and Last Name");   
           }
+ 
+            // This loop will theoretically generate images by pulling down from the server:
+            for (let i = 0; i < response.data.length; i++) {
+ 
+                var artTitle = response.data[i].title;
+                var artistName = response.data[i].creators[0].description;
+                var artistCulture = response.data[i].culture;
+                var artImageURL =  response.data[i].images.web.url;
+
+
+                // Create function to generate elements:
+                function createElements() {
+
+                    // Create Elements for the image card:
+                    var mainImgDiv = $("<div>").attr("class","card m-3" );
+                    var mainImgCardBody = $("<div>").attr("class","card-body");
+                    var cardTitle=$("<h5>").attr("class","card-title").text("Artwork Title: " + artTitle);
+                    var cardName=$("<p>").attr("class","card-text").text("Artist Name: " + artistName);
+                    var cardCulture=$("<p>").attr("class","card-text").text("Artist Ethnicity: " + artistCulture);
+                    var artImageSrc = $("<img>").attr("src", artImageURL).attr("class", "card-img-top");
+                    
+                    // Append Information to the Card Text Box:
+                    mainImgCardBody.append([
+                        cardTitle,
+                        cardName,
+                        cardCulture
+                    ]);
+                    
+                    // Append Image and Card Text Box to the Main Card:
+                    mainImgDiv.append([
+                        artImageSrc,
+                        mainImgCardBody
+                    ]);
+
+                    // Append Card to the a Div element created on the html page.
+                    $(".gallery").append(mainImgDiv);
+                };
+
+                createElements();
+
+        };
+
+        
 
     })
 
